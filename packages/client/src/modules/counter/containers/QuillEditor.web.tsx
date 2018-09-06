6ef1d62ac231af697a8db2c7d864b0c8bd9182e5
lgants <lgants@gmail.com>
2018-09-05 23:17:42 -0500

    cleanup

--- packages/client/src/modules/counter/containers/QuillEditor.web.tsx
+++ packages/client/src/modules/counter/containers/QuillEditor.web.tsx
@@ -15,11 +15,38 @@ export class QuillEditor extends React.Component {
     this.ReactQuill = require('react-quill');
   }
 
+  public handleChange(value: any) {
+    this.setState({ text: value });
+  }
+
   public render() {
     const ReactQuill = this.ReactQuill;
+    const modules = {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+        ['link', 'code', 'image'],
+        ['clean']
+      ]
+    };
+
+    const formats = [
+      'header',
+      'bold',
+      'italic',
+      'underline',
+      'strike',
+      'blockquote',
+      'list',
+      'bullet',
+      'indent',
+      'link',
+      'image'
+    ];
 
     if (ReactQuill) {
-      return <ReactQuill onChange={() => {}} value={this.state.value} />;
+      return <ReactQuill onChange={this.handleChange} value={this.state.value} modules={modules} formats={formats} />;
     } else {
       return <textarea />;
     }
